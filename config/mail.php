84b768b1a07ae99bcb5db732df1a32d68f37aa01
Kenny-xzz <kennedyp479@gmail.com>
2026-09-17 23:53:54 +0200

    Segundo Commit

--- config/mail.php
+++ config/mail.php
@@ -3,4 +3,4 @@ $mail_host = 'smtp.gmail.com';
 $mail_username = 'kennedyp479@gmail.com';       // <- troca pelo teu email
 $mail_password = 'pkktmpkdtvrvuvcr';          // <- troca pela Password de Aplicação (16 caracteres, com espaços tal como o Google gerou)
 $mail_port = 587;
-$mail_from_name = 'SGLE ITC';
+$mail_from_name = 'HELPDESK ITC';
